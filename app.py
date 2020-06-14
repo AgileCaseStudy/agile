8838c23d88a89823e955337d5544b23d873c88af
AgileCaseStudy <66898300+AgileCaseStudy@users.noreply.github.com>
2020-06-14 20:05:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from flask import Flask,render_template,request
 app = Flask(__name__)
 @app.route('/', methods=['GET','POST'])
 def Login_Page():
-    if request.method == 'POST':
-        return render_template('index.html')
+    return render_template('index.html')
     
 @app.route('/create-customer',methods=['GET','POST'])
 def createCustomer():
