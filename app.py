a407a01dfb6c40ea7b636044ba076b77d0b4bfdf
AgileCaseStudy <66898300+AgileCaseStudy@users.noreply.github.com>
2020-06-15 17:51:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from flask import Flask,render_template,request           
 #Importing packages
+from flask import Flask,render_template,request           
+import sqlite3
+import os.path
+
+if os.path.isfile('database.db')==False:
+    conn = sqlite3.connect('database.db')
+    conn.execute('CREATE TABLE Auths (username TEXT Primary Key, password TEXT,User_Type INT)')
+    conn.close()
 
 app = Flask(__name__)
 @app.route('/', methods=['GET','POST'])
